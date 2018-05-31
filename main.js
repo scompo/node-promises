1186450d4e9250fe17764008609b211227c7ef15
Mauro Scomparin <scompo@gmail.com>
2018-05-31 21:52:06 +0200

    testing with Promise#all()

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world!');
+const rp = require('request-promise-native');
+
+const p1 = rp('http://scompo.altervista.org');
+const p2 = rp('http://scompo.altervista.org');
+const p3 = rp('http://scompo.altervista.org');
+
+Promise.all([p1, p2, p3]).then((values) => {
+  for (let i = 0; i < values.length; i++) {
+    const value = values[i];
+    console.log(value);
+  }
+}).catch((err) => {
+  console.error(err);
+});
--- node-wrapper.sh
+++
@@ -1,1 +0,0 @@
-docker run -it --rm --name temp-node -v $(pwd):'/usr/src/app' -w '/usr/src/app' node $@
