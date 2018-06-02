4463273f052bcfb00e69932e3fae68be02818ad6
Mauro Scomparin <scompo@gmail.com>
2018-06-02 20:46:03 +0200

    used promises

--- main.js
+++ main.js
@@ -1,37 +1,31 @@
 const fetch = require('node-fetch');
 
-const p1 = fetch('http://scompo.altervista.org');
 const p2 = Promise.reject(new Error("I wanted this to fail!!"));
 const p3 = new Promise((resolve, reject) => {
   setTimeout(resolve, 3000, 'with a delay');
 });
 
-const errorHandler = (err) => {
+const errorHandler = async (err) => {
   console.error('presumed a network error');
   console.error(err);
   return undefined;
 };
 
-const handleStuff = (values) => {
-  for (let i = 0; i < values.length; i++) {
-    const value = values[i];
-    if (value) {
-      console.log("returned something: " + i);
-    } else {
-      console.log("returned nothing: " + i);
-    }
-  }
+const getStuffWithStatus = async () => {
+  return fetch('https://jsonplaceholder.typicode.com/users')
+    .catch(errorHandler)
+    .then((response) => response.json())
+    .catch(errorHandler)
+    .then((users) => {
+      let superUsers = users.map(async (user) => {
+        const stat = await p2.catch((err) => 'an error');
+        user.stat = stat;
+        return user;
+      });
+      return Promise.all(superUsers);
+    });
 };
 
-const getAllData = (promises, callback) => {
-  Promise.all(promises).then(callback).catch((err) => {
-    console.error('big catch');
-    console.error(err);
-  });
-}
-
-const make3Calls = async () => {
-  getAllData([p1.catch(errorHandler), p2.catch(errorHandler), p3.catch(errorHandler)], handleStuff);
-};
-
-make3Calls();
+const stuff = getStuffWithStatus().then((stuff) => {
+  console.log(stuff);
+}).catch(errorHandler);
